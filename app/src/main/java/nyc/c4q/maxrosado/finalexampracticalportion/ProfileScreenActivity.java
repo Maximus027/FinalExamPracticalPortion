c2e52969334a96080a8631841c57629756b4603f
Max Rosado <max.a.rosado@gmail.com>
2017-02-12 18:18:56 -0500

    Managed to get drawer working but not completely. All buttons are functioning. Did not figure out how to do the instructions for the Settings Activity in time. Need to complete this part at a later time.

--- app/src/main/java/nyc/c4q/maxrosado/finalexampracticalportion/ProfileScreenActivity.java
+++ app/src/main/java/nyc/c4q/maxrosado/finalexampracticalportion/ProfileScreenActivity.java
@@ -1,13 +1,85 @@
 package nyc.c4q.maxrosado.finalexampracticalportion;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class ProfileScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+        setContentView(R.layout.activity_profile_screen);
+        NavigationView navigationMenu = (NavigationView) findViewById(R.id.profile_nav_drawer);
+        setupDrawerContent(navigationMenu);
+
+    }
+
+    public void setHeartButton(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Selected Heart Activity", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void setMessageButton(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Selected Message Activity", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void setSettingsButton (View view) {
+        Intent intent = new Intent(this, SettingsScreenActivity.class);
+        startActivity(intent);
+    }
+
+    public void setLogOffButton (View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Selected Logout Activity", Toast.LENGTH_SHORT);
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        toast.show();
+        startActivity(intent);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    private void selectDrawerItem(MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.heart_nav_button:
+                Toast toast1 = Toast.makeText(getApplicationContext(), "Selected Heart Activity", Toast.LENGTH_SHORT);
+                toast1.show();
+                break;
+            case R.id.message_nav_button:
+                Toast toast2 = Toast.makeText(getApplicationContext(), "Selected Message Activity", Toast.LENGTH_SHORT);
+                toast2.show();
+                break;
+            case R.id.settings_nav_button:
+                Intent intent1 = new Intent(this, SettingsScreenActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.log_out_nav_button:
+                Toast toast = Toast.makeText(getApplicationContext(), "Selected Logout Activity", Toast.LENGTH_SHORT);
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                toast.show();
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/nyc/c4q/maxrosado/finalexampracticalportion/SettingsScreenActivity.java
+++ app/src/main/java/nyc/c4q/maxrosado/finalexampracticalportion/SettingsScreenActivity.java
@@ -1,7 +1,13 @@
 package nyc.c4q.maxrosado.finalexampracticalportion;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SettingsScreenActivity extends AppCompatActivity {
 
@@ -9,5 +15,51 @@ public class SettingsScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_screen);
+
+        RecyclerView settingsRecyclerView = (RecyclerView) findViewById(R.id.activity_settings_screen);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        settingsRecyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    public class Setting {
+        private String setting;
+
+        public Setting (int number) {
+            
+        }
+    }
+
+    public class SettingsViewHolder extends RecyclerView.ViewHolder {
+        private TextView tv1;
+        private RecyclerView.ViewHolder settingsVH;
+        private RecyclerView settingsRV;
+
+        public SettingsViewHolder(View itemView) {
+            super(itemView);
+            tv1 = (TextView) itemView.findViewById(R.id.settings_tv);
+        }
+
+        public void bind() {
+        }
+    }
+
+    public class SettingsAdapter extends RecyclerView.Adapter<SettingsViewHolder> {
+        private Context context;
+
+
+        @Override
+        public SettingsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(SettingsViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
