44321a9eb64cc6999ee1fdde578acde145bdfa3e
Max Rosado <max.a.rosado@gmail.com>
2017-02-12 10:58:18 -0500

    Added blank activities. Added 2 EditTexts and 1 Button for LogInActivity.

--- app/src/main/java/nyc/c4q/maxrosado/finalexampracticalportion/SettingsScreenActivity.java
+++ app/src/main/java/nyc/c4q/maxrosado/finalexampracticalportion/SettingsScreenActivity.java
@@ -3,11 +3,11 @@ package nyc.c4q.maxrosado.finalexampracticalportion;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingsScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings_screen);
     }
 }
