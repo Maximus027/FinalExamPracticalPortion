3b88412399666fa4902b6b25e7c2a42f281afe81
Max Rosado <max.a.rosado@gmail.com>
2017-02-12 11:44:46 -0500

    Added methods and logic for submit button, everything seems to be in working order for login activity

--- app/src/main/java/nyc/c4q/maxrosado/finalexampracticalportion/LogInActivity.java
+++ app/src/main/java/nyc/c4q/maxrosado/finalexampracticalportion/LogInActivity.java
@@ -1,7 +1,11 @@
 package nyc.c4q.maxrosado.finalexampracticalportion;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void setSubmitButton(View view) {
+        EditText usernameET = (EditText) findViewById(R.id.username_et);
+        EditText passwordET = (EditText) findViewById(R.id.password_et);
+
+        String username = usernameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        if (username == "" || username == null || username.isEmpty() || username.length() == 0) {
+            noUsernameToast(view);
+        }
+        if (password == "" || password == null || username.isEmpty() || username.length() == 0) {
+            noPasswordToast(view);
+        }
+        if (username.length() != 0 && password.length() != 0) {
+            goToMainScreenActivity(view);
+        }
+
+    }
+
+    public void goToMainScreenActivity(View view) {
+        Intent intent = new Intent(this, MainScreenActivity.class);
+        startActivity(intent);
+    }
+
+    public void noUsernameToast(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "No Username entered!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void noPasswordToast(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "No Password entered!", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
